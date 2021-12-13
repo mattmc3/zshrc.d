e4081f35f7738d39d33d6a49cbda016865c12c50
mattmc3 <mattmc3@gmail.com>
2021-12-13 10:09:51 -0500

    Simplify implementation

--- zshrc.d.zsh
+++ zshrc.d.zsh
@@ -1,36 +1,21 @@
-function source_conf_dir() {
-  # The config dir is determined as follows:
-  # - confdir argument provided
-  # - the zstyle setting is set (ie: zstyle ':zshrc.d:*' 'path' ~/path/to/my/custom/zshrc.d)
-  # - or the default location is used (ie: ${ZDOTDIR:-$HOME}/.zshrc.d)
-  local confdir
-  if [[ $# -gt 0 ]]; then
-    confdir="$1"
-  else
-    zstyle -s ':zshrc.d:*' 'path' confdir || {
-      confdir="${ZDOTDIR:-$HOME}/.zshrc.d"
-      if [[ ! -d "$confdir" ]] && [[ -d "${ZDOTDIR:-~/.config/zsh}/zshrc.d" ]]; then
-        confdir="${ZDOTDIR}/zshrc.d"
-      fi
-    }
-  fi
-
-  # make sure we found the conf dir
-  [[ -d "$confdir" ]] || {
-    >&2 echo "source_conf_dir: config dir not found $confdir" && return 1
-  }
-
-  # source all files in confdir
-  local files=("$confdir"/*.{sh,zsh}(N))
-  local f; for f in ${(o)files}; do
-    # ignore files that begin with a tilde
-    case ${f:t} in '~'*) continue;; esac
-    source "$f"
-  done
-}
-
 () {
-  local defer_confdir
-  zstyle -b ':zshrc.d:*' 'defer' defer_confdir || defer_confdir=no
-  [[ $defer_confdir = yes ]] || source_conf_dir
+    # determine zshrc.d
+    if [[ -z "$ZSHRCD" ]]; then
+        ZSHRCD="${ZDOTDIR:-$HOME}/.zshrc.d"
+        if [[ ! -d "$ZSHRCD" ]] && [[ -d "$ZDOTDIR/zshrc.d" ]]; then
+            ZSHRCD="$ZDOTDIR/zshrc.d"
+        fi
+    fi
+    # make sure we found the zshrc.d
+    if [[ ! -d "$ZSHRCD" ]]; then
+        echo >&2 "zshrc.d: dir not found '$ZSHRCD'" && return 1
+    fi
+    # source all files in zshrc.d
+    local conf_files=("$ZSHRCD"/*.{sh,zsh}(N))
+    local f
+    for f in ${(o)conf_files}; do
+        # ignore files that begin with a tilde
+        case ${f:t} in '~'*) continue;; esac
+        source "$f"
+    done
 }
